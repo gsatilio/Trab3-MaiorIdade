f2657df24f9feae8119bfef82b63248706253c33
Guilherme <Guilherme@DESKTOP-V4NM6ON>
2024-04-17 17:04:34 -0300

    Add project files.

---
+++ Trab3-MaiorIdade/Program.cs
@@ -0,0 +1,17 @@
+﻿//3 - faça um algoritmo que leia a idade de uma pessoa e informe se ela é maior ou menor de idade
+int idade;
+
+Console.WriteLine("Por favor, informe a sua idade.");
+
+idade = int.Parse(Console.ReadLine());
+
+if(idade >= 18)
+{
+    Console.WriteLine("Parabens, voce eh maior de idade!");
+}
+else
+{
+    Console.WriteLine("Que pena, voce eh menor de idade!");
+}
+
+Console.ReadKey();
